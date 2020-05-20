f334844a9b083f77f68a26236dc80d599f4effa5
GermanVq <gavegaq@gmail.com>
2020-05-20 18:44:43 -0500

    created routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router'
+import { HospitalComponent } from './components/hospital/hospital.component';
+import { DoctorComponent } from './components/doctor/doctor.component';
+import { TriageComponent } from './components/triage/triage.component';
+import { PacienteComponent } from './components/paciente/paciente.component';
+import { HomeComponent } from './components/home/home.component';
+
+const rutas: Routes = [
+  {
+    path: 'hospital',
+    component: HospitalComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'doctor',
+    component: DoctorComponent
+  },
+  {
+    path: 'paciente',
+    component: PacienteComponent
+  },
+  {
+    path: 'triage',
+    component: TriageComponent
+  },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HospitalComponent,
+    DoctorComponent,
+    TriageComponent,
+    PacienteComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
